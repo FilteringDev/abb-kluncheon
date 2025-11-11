37f1aff761d62ec4eea52c465cd11c644149497f
PiQuark6046 <j.w.so@adguard.com>
2025-11-11 19:18:00 +0000

    fix: create a Promise trap to prevent execution of anti-adblock script

--- sources/src/index.ts
+++ sources/src/index.ts
@@ -4,16 +4,17 @@ declare const unsafeWindow: unsafeWindow
 
 const Win = typeof unsafeWindow !== 'undefined' ? unsafeWindow : window
 
-let AdblockDetectPositiveRegExps: RegExp[][] = [[
+let AdblockDetectInnerPromiseTrapRegExps: RegExp[][] = [[
   /if *\( *typeof *window *\[ *\w+\( *\d+ *\) *\] *!== *\w+\( *\d+ *\) *\) *{/,
   /const *\w+ *= *setTimeout *\( *\( *\) *=> *{/,
-  /typeof *\w+ *!== *\w+ *\( *[\da-f]+ *\) *\|\| *\w+ *===? *void *0 *\|\| *\w+ *===? *null *\|\| *\w+ *===? *('|")+ *\)/
+  /const *\w *= *window *\[ *\w *\( *\d+ *\) *\] *\[ *\w+ *\( *\d+ *\) *\] *\( *\) *;/,
+  / *\w+ *\( *! *\w+ *\|\| *\w+ *=== *"" *\) *} *catch *{ *clearTimeout *\( *\w+ *\) *, *\w+ *\( */
 ]]
 
 Win.Promise = new Proxy(Win.Promise, {
   construct(Target: typeof Promise, Args: ConstructorParameters<typeof Promise>) {
-    if (typeof Args[0] === 'function' && AdblockDetectPositiveRegExps.filter(RegExpList => RegExpList.filter(Reg => Reg.test(Args[0].toString())).length >= 3).length === 1) {
-      return new Promise(async () => {})
+    if (typeof Args[0] === 'function' && AdblockDetectInnerPromiseTrapRegExps.filter(RegExpList => RegExpList.filter(Reg => Reg.test(Args[0].toString())).length >= 4).length === 1) {
+      return Reflect.construct(Target, [() => {}])
     }
     return Reflect.construct(Target, Args)
   }
